cc055877812eda901691393772c5e8a63ce55e4b
Alexey <70944468+Alexey-Samodurov@users.noreply.github.com>
2023-06-25 16:31:41 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ docker-compose up superset-init --build
 docker-compose up -d
 echo Wait for connections ready...
 sleep 30
+echo Airflow ready on: http://localhost:8080
+echo Superset ready on: http://localhost:8088
